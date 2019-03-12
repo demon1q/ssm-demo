3c88a3cfa681cddb318a1d3c173d3eab2feb86b9
Demon <18112358325@163.com>
2019-03-12 13:04:56 +0800

    登录

--- src/main/java/cn/demon/controller/UserController.java
+++ src/main/java/cn/demon/controller/UserController.java
@@ -1,10 +1,55 @@
 package cn.demon.controller;
 
+import cn.demon.pojo.User;
+import cn.demon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 /**
  * @desciption: 用户控制器
  * @author: Demon
  * @version: 1.0 2019-03-11 12:26
  **/
+@Controller
+@RequestMapping(value = "/user")
 public class UserController {
+    private final UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * @param username
+     * @param password
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        User user = userService.login(username);
+        if (user == null) {
+            model.addAttribute("message", "用户不存在");
+            return "page/loginInfo";
+        }
+        if (!user.getPassword().equals(password)) {
+            model.addAttribute("message", "密码错误");
+            return "page/loginInfo";
+        }
+        return "page/page";
+    }
+
+    /**
+     * 回到登录页
+     */
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
 }
--- src/main/java/cn/demon/service/impl/UserServiceImpl.java
+++ src/main/java/cn/demon/service/impl/UserServiceImpl.java
@@ -1,20 +1,27 @@
 package cn.demon.service.impl;
 
+import cn.demon.dao.UserMapper;
 import cn.demon.pojo.User;
 import cn.demon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @desciption: UserService实现类
  * @author: Demon
  * @version: 1.0 2019-03-11 14:14
  **/
+@Service
 public class UserServiceImpl implements UserService {
+    private final UserMapper userMapper;
+
     @Autowired
-    private UserService userService;
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     @Override
     public User login(String username) {
-        return null;
+        return userMapper.selectByName(username);
     }
 }
