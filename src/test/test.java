cc4c1a8583ee53125a971547f6c49e4835d69370
Demon <18112358325@163.com>
2019-03-12 13:07:36 +0800

    登录

---
+++ src/test/test.java
@@ -0,0 +1,28 @@
+import cn.demon.dao.UserMapper;
+import cn.demon.pojo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+
+/**
+ * @desciption:
+ * @author: Demon
+ * @version: 1.0 2019-03-11 14:36
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/beans.xml")
+public class test {
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void select(){
+        User user = userMapper.selectByName("111");
+//        UserService userService = new UserServiceImpl();
+//        User user = userService.login("admin");
+        System.out.println(user);
+    }
+}
